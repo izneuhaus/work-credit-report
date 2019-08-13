154a099b8afd94e3b4e64e16d749c8fd6f8c2279
iznaut <izneuhaus@gmail.com>
2019-08-12 22:05:20 -0500

    Added personalized report pages

--- main.js
+++ main.js
@@ -36,6 +36,15 @@ $( document ).ready(function() {
         mounted: function(){
             this.loadItems();
         },
+        updated: function() {
+            $('.collapse').each(function(index, collapse) {
+                var id = $(collapse).get(0).id;
+
+                if (localStorage.getItem(id) === 'false') {
+                    collapseOnLoad(id);
+                }
+            });
+        },
         methods: {
             loadItems: function(){
     
@@ -111,6 +120,18 @@ $( document ).ready(function() {
 
                         self[tableName] = formattedData;
                     });
+
+                    var url = window.location.pathname;
+                    var filename = url.substring(url.lastIndexOf('/')+1);
+
+                    if (filename == 'dashboard.html') {
+                        if (getUrlParameter('id') && getUrlParameter('house')) {
+                            var memberId = getUrlParameter('id');
+                            var memberHouse = getUrlParameter('house');
+                    
+                            loadPersonalizedView(memberId, memberHouse)
+                        }
+                    }
                 })
                 .catch(function(error){
                     console.log(error)
@@ -149,25 +170,135 @@ $( document ).ready(function() {
         window.open(app._data["Report Configuration"].formUrl);
     });
 
+    $('#backToReport > button').click(function() {
+        window.location.href="./index.html";
+    });
+
     $('.nav-link').click(function() {
         var id = $(this).attr('id');
 
         if (id !== 'help') {
-            var reportView = $('#reportView')
-            var submissionView = $('#submissionsView')
+            var reportView = $('#reportView');
+            var submissionView = $('#submissionsView');
+            var quickSubmitView = $('#quickSubmitView');
 
             $('.nav').find('.active').removeClass('active');
             $(this).addClass('active');
 
             if (id == 'history') {
                 reportView.hide();
+                quickSubmitView.hide();
                 submissionView.show();
             }
             else if (id == 'report') {
-                reportView.show();
                 submissionView.hide();
+                quickSubmitView.hide();
+                reportView.show();
             }
         }
+    });    
+
+    $('#reportView').on('click', '.quick-submit', function() {
+        var memberId = $(this).data('id');
+        var memberHouse = $(this).data('house');
+
+        window.location.href='./dashboard.html?id=' + memberId + '&house=' + memberHouse;
+
+        // loadPersonalizedView(memberId, memberHouse)
+    });
+
+    $('#reportView').on('shown.bs.collapse', function (e) {
+        localStorage.setItem(e.target.id, true);
+    });
+
+    $('#reportView').on('hidden.bs.collapse', function (e) {
+        localStorage.setItem(e.target.id, false);
+    });
+
+    $('#survey').on('load', function() {
+        $('.submitButton').click(function () {
+            console.log('test')
+        })
     })
 });
 
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+function collapseOnLoad(id) {
+    $('#' + id).removeClass('show');
+}
+
+function loadPersonalizedView(memberId, memberHouse) {
+    var header = $('#header');
+    var reportView = $('#reportView');
+    var submissionView = $('#submissionsView');
+    var quickSubmitView = $('#quickSubmitView');
+
+    var memberData = app._data['Member Lookup'][memberHouse][memberId];
+
+    $('#submitDashboard').click(function() {
+        window.location.href = 
+            'https://airtable.com/embed/shr6hdgPnHm7pXmsz?backgroundColor=red' +
+            '&prefill_Member=' + memberData['Name'] +
+            '&prefill_House=' + memberData['House'];
+    });
+
+    $('.hoursProgressDisplay').each(function(index, item) {
+        var hoursType = $(item).data('hourstype');
+        var progressDisplay = new ldBar(item, {
+            'preset': 'circle'
+        });
+        var label = $(item).find('.ldBar-label');
+
+        var currentHours = app.getCurrentHours(memberData, hoursType);
+        var requiredHours = memberData['Required ' + hoursType + ' Hours (' + (hoursType === 'Maintenance' ? 'Yearly' : 'Monthly') + ')'];
+
+        var progressPercentage = (currentHours / requiredHours) * 100;
+
+        progressDisplay.set(progressPercentage);
+
+        $(item).find('.progress-label').remove();
+        $('<div class="progress-label">' + (progressPercentage > 100 ? requiredHours : currentHours) + '/' + requiredHours + '</div>').appendTo($(item));
+    })
+
+    $('.otherProgressDisplay').each(function(index, item) {
+        var otherType = $(item).data('othertype');
+        var progressDisplay = new ldBar(item, {
+            'preset': 'circle',
+
+        });
+        var label = $(item).find('.ldBar-label');
+
+        var currentOtherProgress = 1;
+        var otherRequirement = memberData['Required ' + otherType + ' (Monthly)'];
+
+        var progressPercentage = (currentOtherProgress / otherRequirement) * 100;
+
+        progressDisplay.set(progressPercentage)
+        $(item).find('.progress-other-label').remove();
+        $('<div class="progress-other-label">' + currentOtherProgress + '/' + otherRequirement + '</div>').appendTo($(item));
+    })
+
+    var date = new Date;
+    date = date.toLocaleString('default', { month: 'long', year: 'numeric' })
+
+    $('#quickSubmitHeader').html('<h4>' + memberData['Name'] + '</h4><h5>' + date + '</h5>');
+
+    header.hide();
+    reportView.hide();
+    submissionView.hide();
+    quickSubmitView.show();
+}
